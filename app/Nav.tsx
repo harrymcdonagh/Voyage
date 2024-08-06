240911606fc15c4aa8db14222574b04ef75693a4
Harry McDonagh <harrymcdonagh82@gmail.com>
2024-08-07 00:02:11 +0200

    Updated Navbar

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -9,6 +9,14 @@ import { IoRocketOutline } from "react-icons/io5";
 import { MdShowChart } from "react-icons/md";
 import { FiPieChart, FiStar } from "react-icons/fi";
 import { useSession } from "next-auth/react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
 
 export default function Nav() {
   const { status, data: session } = useSession();
@@ -29,22 +37,27 @@ export default function Nav() {
             <MdShowChart className="h-5 w-5" />
             Prices
           </Link>
-          <Link
-            href="/portfolio"
-            className="flex items-center gap-2 transition-colors hover:text-primary hover:underline hover:underline-offset-4"
-            prefetch={false}
-          >
-            <FiPieChart className="h-5 w-5" />
-            Portfolio
-          </Link>
-          <Link
-            href="/watchlist"
-            className="flex items-center gap-2 transition-colors hover:text-primary hover:underline hover:underline-offset-4"
-            prefetch={false}
-          >
-            <FiStar className="h-5 w-5" />
-            Watchlist
-          </Link>
+          {status === "authenticated" && (
+            <>
+              <Link
+                href="/portfolio"
+                className="flex items-center gap-2 transition-colors hover:text-primary hover:underline hover:underline-offset-4"
+                prefetch={false}
+              >
+                <FiPieChart className="h-5 w-5" />
+                Portfolio
+              </Link>
+              <Link
+                href="/watchlist"
+                className="flex items-center gap-2 transition-colors hover:text-primary hover:underline hover:underline-offset-4"
+                prefetch={false}
+              >
+                <FiStar className="h-5 w-5" />
+                Watchlist
+              </Link>
+            </>
+          )}
+
           {status === "loading" && <div>Loading...</div>}
           {status === "unauthenticated" && (
             <div className="flex items-center gap-4">
@@ -56,7 +69,33 @@ export default function Nav() {
             </div>
           )}
           {status === "authenticated" && (
-            <div className="flex items-center">{session.user!.name}</div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Avatar className="h-9 w-9">
+                  <AvatarImage
+                    src={`${session.user!.image}`}
+                    alt="@shadcn"
+                    className="rounded-full hover:opacity-80 cursor-pointer"
+                  />
+                  <AvatarFallback className="rounded-full bg-primary flex items-center justify-center">
+                    {session.user!.name![0]}
+                  </AvatarFallback>
+                  <span className="sr-only">Toggle user menu</span>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <Link href="/">My Account</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href="/">Settings</Link>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link href="/api/auth/signout">Logout</Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           )}
         </nav>
         <Sheet>
@@ -69,8 +108,8 @@ export default function Nav() {
           <SheetContent side="right" className="w-full max-w-xs">
             <div className="flex h-16 items-center justify-between px-4">
               <Link href="/" className="flex items-center gap-2" prefetch={false}>
-                <IoRocketOutline className="h-6 w-6" />
-                <span className="text-lg font-semibold">Voyage</span>
+                <IoRocketOutline className="h-7 w-7" />
+                <span className="text-xl font-semibold">Voyage</span>
               </Link>
               <div className="flex gap-1">
                 <ModeToggle />
@@ -83,36 +122,65 @@ export default function Nav() {
               </div>
             </div>
             <nav className="grid gap-4 px-4 py-6">
+              {status === "unauthenticated" && (
+                <div>
+                  <Link href="/api/auth/signin" className="flex flex-col mb-2">
+                    <Button variant="outline">Sign In</Button>
+                  </Link>
+                  <Link href="/api/auth/signin" className="flex flex-col">
+                    <Button>Register</Button>
+                  </Link>
+                </div>
+              )}
+              {status === "authenticated" && (
+                <Link
+                  href="/account"
+                  className="flex items-center gap-2 text-md font-medium transition-colors hover:text-primary"
+                >
+                  <Avatar className="h-7 w-7">
+                    <AvatarImage
+                      src={`${session.user!.image}`}
+                      alt="@shadcn"
+                      className="rounded-full hover:opacity-80 cursor-pointer"
+                    />
+                    <AvatarFallback className="rounded-full bg-primary flex items-center justify-center">
+                      {session.user!.name![0]}
+                    </AvatarFallback>
+                  </Avatar>
+                  Profile
+                </Link>
+              )}
               <Link
                 href="/prices"
-                className="flex items-center gap-2 text-sm font-medium transition-colors hover:text-primary"
+                className="flex items-center gap-2 text-md font-medium transition-colors hover:text-primary"
                 prefetch={false}
               >
-                <MdShowChart className="h-5 w-5" />
+                <MdShowChart className="h-7 w-7" />
                 Prices
               </Link>
-              <Link
-                href="/portfolio"
-                className="flex items-center gap-2 text-sm font-medium transition-colors hover:text-primary"
-                prefetch={false}
-              >
-                <FiPieChart className="h-5 w-5" />
-                Portfolio
-              </Link>
-              <Link
-                href="/watchlist"
-                className="flex items-center gap-2 text-sm font-medium transition-colors hover:text-primary"
-                prefetch={false}
-              >
-                <FiStar className="h-5 w-5" />
-                Watchlist
-              </Link>
-              <div className="flex flex-col gap-2">
-                <Link href="/api/auth/signin">
-                  <Button variant="outline">Sign In</Button>
-                </Link>
-                <Button>Register</Button>
-              </div>
+              {status === "authenticated" && (
+                <>
+                  <Link
+                    href="/portfolio"
+                    className="flex items-center gap-2 text-md font-medium transition-colors hover:text-primary"
+                    prefetch={false}
+                  >
+                    <FiPieChart className="h-7 w-7" />
+                    Portfolio
+                  </Link>
+                  <Link
+                    href="/watchlist"
+                    className="flex items-center gap-2 text-md font-medium transition-colors hover:text-primary"
+                    prefetch={false}
+                  >
+                    <FiStar className="h-7 w-7" />
+                    Watchlist
+                  </Link>
+                  <Link href="/api/auth/signout" className="flex flex-col gap-2">
+                    <Button>Logout</Button>
+                  </Link>
+                </>
+              )}
             </nav>
           </SheetContent>
         </Sheet>
